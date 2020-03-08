103172bede1b3fbfb0b64b83df60191bd9754c86
BuildTools <unconfigured@null.spigotmc.org>
2020-03-07 20:13:45 -0500

    Reorganized a little

--- Bread_Bot/breadBot.py
+++ Bread_Bot/breadBot.py
@@ -8,7 +8,7 @@ import asyncio
 from datetime import datetime
 import urllib.request
 import random
-from config import TOKEN
+from config import TOKEN, CHANNEL_ID
 
 client = discord.Client()
 
@@ -17,43 +17,8 @@ client = discord.Client()
 async def on_message(message):
     channel = message.channel
     if message.content == "TEST":
-        fp = urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_breads")
-        mybytes = fp.read()
-        mystr = mybytes.decode("utf8")
-        fp.close()
-        index1 = mystr.find('<tbody>')
-        index2 = mystr.find('</tbody>')
-        if index1 > 0:
-            # isolate the table
-            mystr = mystr[index1+7:index2]
-
-            # remove header row
-            index1 = mystr.find("</tr>")
-            mystr = mystr[index1+6:]
-
-            # divide the the table into rows
-            mystr = mystr.split('<tr>')
-
-            # pick a random row and remove it
-            rand = random.randint(0, len(mystr)-1)
-            row = mystr[rand]
-
-            # find and remove the name of the bread
-            name_index1 = row.find('">') + 2
-            name_index2 = row.find('</a>')
-            bread_name = row[name_index1:name_index2]
-
-            # find and save the link
-            bread_link = 'https://en.wikipedia.org/'
-            link_index1 = row.find('wiki')
-            link_index2 = row.find('" title')
-            bread_link += row[link_index1:link_index2]
-
-            # create the final message and send it
-            msg = "Good morning everyone! Rise and grind, let's get this bread. Today's bread is "
-            msg += bread_name + '!\n('
-            msg += bread_link + ')'
-            await channel.send(msg)
+        msg = get_bread()
+        await channel.send(msg)
 
     # the bread reaction method
     if "bread" in message.content or "Bread" in message.content:
@@ -65,54 +30,56 @@ async def on_ready():
     print("Logged in as")
     print(client.user.name)
     print(client.user.id)
-    channel = client.get_channel(459365635093299201)
+    channel = client.get_channel(CHANNEL_ID)
     while True:
         now = datetime.now()
         hour = int(now.strftime("%H"))
         if hour == 8:
             # unleash the bread
-            fp = urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_breads")
-            mybytes = fp.read()
-            mystr = mybytes.decode("utf8")
-            fp.close()
-            index1 = mystr.find('<tbody>')
-            index2 = mystr.find('</tbody>')
-            if index1 > 0:
-                # isolate the table
-                mystr = mystr[index1 + 7:index2]
-
-                # remove header row
-                index1 = mystr.find("</tr>")
-                mystr = mystr[index1 + 6:]
-
-                # divide the the table into rows
-                mystr = mystr.split('<tr>')
-
-                # pick a random row and remove it
-                rand = random.randint(0, len(mystr) - 1)
-                row = mystr[rand]
-
-                # find and remove the name of the bread
-                name_index1 = row.find('">') + 2
-                name_index2 = row.find('</a>')
-                bread_name = row[name_index1:name_index2]
-
-                # find and save the link
-                bread_link = 'https://en.wikipedia.org/'
-                link_index1 = row.find('wiki')
-                link_index2 = row.find('" title')
-                bread_link += row[link_index1:link_index2]
-
-                # create the final message and send it
-                msg = "Good morning everyone! Rise and grind, let's get this bread. Today's bread is "
-                msg += bread_name + '!\n('
-                msg += bread_link + ')'
-                await channel.send(msg)
+            bread_message = get_bread()
+            await channel.send(bread_message)
             await asyncio.sleep(3600)
         else:
             await asyncio.sleep(60)
 
+def get_bread():
+        fp = urllib.request.urlopen("https://en.wikipedia.org/wiki/List_of_breads")
+        mybytes = fp.read()
+        mystr = mybytes.decode("utf8")
+        fp.close()
+        index1 = mystr.find('<tbody>')
+        index2 = mystr.find('</tbody>')
+        if index1 > 0:
+            # isolate the table
+            mystr = mystr[index1 + 7:index2]
 
+            # remove header row
+            index1 = mystr.find("</tr>")
+            mystr = mystr[index1 + 6:]
+
+            # divide the the table into rows
+            mystr = mystr.split('<tr>')
+
+            # pick a random row and remove it
+            rand = random.randint(0, len(mystr) - 1)
+            row = mystr[rand]
+
+            # find and remove the name of the bread
+            name_index1 = row.find('">') + 2
+            name_index2 = row.find('</a>')
+            bread_name = row[name_index1:name_index2]
+
+            # find and save the link
+            bread_link = 'https://en.wikipedia.org/'
+            link_index1 = row.find('wiki')
+            link_index2 = row.find('" ')
+            bread_link += row[link_index1:link_index2]
+
+            # create the final message and send it
+            msg = "Good morning everyone! Rise and grind, let's get this bread. Today's bread is "
+            msg += bread_name + '!\n('
+            msg += bread_link + ')'
+            return msg
 
 
 client.run(TOKEN)
