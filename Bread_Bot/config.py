63e0f1c78facf61ff0b983c9b129eb72c013296c
Alex Sutay <ads1429@rit.edu>
2020-06-06 16:40:42 -0400

    Default config file

---
+++ Bread_Bot/config.py
@@ -0,0 +1,4 @@
+# Default config file. Replace token string with string from the discord developer portal and channel id of the channel
+# you want the bread message sent to
+TOKEN = ""
+CHANNEL_ID = 0
---
+++ savagebot/config.py
@@ -0,0 +1,2 @@
+# Default config file. Replace token string with string from the discord developer portal
+TOKEN = ""
