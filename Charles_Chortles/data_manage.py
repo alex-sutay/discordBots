83199cc19e63d1ac22b7d47721e74b5067d4179c
Alex Sutay <ads1429@rit.edu>
2020-06-06 16:36:26 -0400

    Now should support ranking anyone

--- Charles_Chortles/data_manage.py
+++ Charles_Chortles/data_manage.py
@@ -3,7 +3,9 @@ author: Alex Sutay
 file: data_manage.py
 """
 
-FILENAME = "/home/AlexS/discordbots/Charles_Chortles/im_instances"
+
+class AuthorException(Exception):
+    pass
 
 
 def add_entry():
@@ -11,7 +13,8 @@ def add_entry():
     set the focus on the next image, committing the current instance
     :return: None
     """
-    f = open(FILENAME,"a")
+    filename = get_filename()
+    f = open(filename, "a+")
     f.write("\nfill:0")
     f.close()
 
@@ -21,7 +24,7 @@ def total_current():
     find the total score so far of the current image
     :return: score as a float
     """
-    f = open(FILENAME)
+    f = open(get_filename())
     lines = f.readlines()
     current = lines[-1].split(',')
     total = 0
@@ -39,7 +42,7 @@ def find_average():
     """
     total_num = 0
     score = 0
-    f = open(FILENAME)
+    f = open(get_filename())
     for line in f:
         total_num += 1
         line = line.split(',')
@@ -57,8 +60,11 @@ def add_to_current(userid, score):
     :param userid: The id of the user submitting the score
     :param score: the score that user wants to give it
     """
+    author = get_filename()
+    if author == userid[-5:]:
+        raise AuthorException
     scores = dict()
-    f = open(FILENAME)
+    f = open(author)
     lines = f.readlines()
     f.close()
     this = lines[-1].split(',')
@@ -72,7 +78,7 @@ def add_to_current(userid, score):
     for ids in scores:
         new_str += str(ids) + ':' + str(scores[ids]) + ','
     new_str = new_str[:-1]
-    f = open(FILENAME, "a")
+    f = open(author, "a")
     f.write(new_str)
     f.close()
 
@@ -81,11 +87,11 @@ def remove_last_line():
     """
     removes the last line of the working file
     """
-    f = open(FILENAME)
+    f = open(get_filename())
     lines = f.readlines()
     f.close()
     lines = lines[:-1]
-    f = open(FILENAME,"w")
+    f = open(get_filename(), "w")
     for item in range(0, len(lines)):
         f.write(lines[item])
 
@@ -95,7 +101,7 @@ def scores_str():
     Return a string containing all of the scores in a string
     :return: string containing scores
     """
-    f = open(FILENAME)
+    f = open(get_filename())
     lines = f.readlines()
     f.close()
     ret_str = ""
@@ -122,6 +128,20 @@ def scores_str():
 def remove_last_input():
     remove_last_line()
     remove_last_line()
-    f = open(FILENAME, "a")
+    f = open(get_filename(), "a")
     f.write("fill:0")
     f.close()
+
+
+def set_current(author):
+    f = open("current", 'w')
+    f.write(str(author)[-5:])
+    f.close()
+    add_entry()
+
+
+def get_filename():
+    f = open("current")
+    rtnstr = f.read()
+    f.close()
+    return rtnstr
